82c1281684b95d7e0ab95f5176bd5c03b39dd5f1
SUMANTH PIKKILI <sumanthpikkili@SUMANTHs-MacBook-Pro-3.local>
2015-11-14 22:09:08 -0600

    Commit 5

--- retrieve_mongo.rb
+++ retrieve_mongo.rb
@@ -6,14 +6,21 @@ mongo_uri = 'mongodb://heroku_8c77z5bn:s650vnnnpv39p3h8epm0jima98@ds053764.mongo
 begin
     client = Mongo::Client.new([ 'ds053764.mongolab.com:53764' ],:database =>'heroku_8c77z5bn',:user=>'heroku_8c77z5bn',:password=>'s650vnnnpv39p3h8epm0jima98');
     puts "connected to mongo db"
-    db = client.database
-    college=db.collection('universities')
     puts "Enter the primary key value(UNITID)"
     pk = gets
+    if pk.strip! == nil
+	pkey=pk
+    else
+         pkey=pk
+    end
+    db = client.database
+    college=db.collection('universities')
+    
    
 	puts "Retrieve successful"
 	puts "The retrieved data for the primary key value entered is"
-	result = college.find({ :unitid =>pk })              
+	result = college.find({ :unitid =>pkey })
+	puts "UNITID | OPEID | OPEID6 | INSTNM | CITY | STABBR | INSTURL              
         result.each do |document|
   	puts "#{document['unitid']} | #{document['opeid']} | #{document['opeid6']} | #{document['instnm']} | #{document['city']} | #{document['stabbr']} | #{document['insturl']}" 
 	
