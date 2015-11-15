9a315c7961f83c53446e059cd44e9a68e6739f36
SUMANTH PIKKILI <sumanthpikkili@SUMANTHs-MacBook-Pro-3.local>
2015-11-14 22:41:33 -0600

    Commit12

---
+++ retrieve_nonprimary.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+
+require 'csv'
+require 'uri'
+require 'pg'
+
+uri = URI.parse('postgres://vbzzdosloeskke:8DPMBrS0qFPfF5gRY6MkFvFN9H@ec2-107-21-223-110.compute-1.amazonaws.com:5432/deb8hqfv9kd04o')
+
+begin
+        connection = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+        puts 'connected'
+        puts "Enter the Non Primary Key value (1. STABBR 2. CITY)"
+        option = gets
+
+        puts case option
+	when 1
+		col_name = "stabbr"
+	when 2
+		col_name = "city"
+	else
+		"You have chosen the wrong option"
+	end
+	
+	puts "Enter the value for the Non Primarykey chosen"
+	nonpk=gets
+
+        query = connection.exec("select * from universities where" +  col_name + " =" + nonpk + "" + "limit 20;");
+        puts "The following are your search results based on the non Primary key value that you have given for stabbr"
+        query.each {
+        unitid=query[0]['unitid']
+        opeid=query[0]['opeid']
+        opeid6=query[0]['opeid6']
+        instnm=query[0]['instnm']
+        city=query[0]['city']
+        stabbr=query[0]['stabbr']
+        insturl=query[0]['insturl']
+
+        p "#{unitid} #{opeid} #{opeid6} #{instnm} #{city} #{stabbr} #{insturl}"
+        }
+
+        puts "success"
+        connection.close
+end
